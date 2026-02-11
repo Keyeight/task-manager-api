c8ba407ba8ba1714a544afe5a815788bc83162a8
Keyth Alves <keythalvesferreia@gmail.com>
2026-02-11 03:57:18 -0300

    refactor: adjust name and retreat

--- src/middleware/validate-schema.middleware.ts
+++ src/middleware/validate-schema.middleware.ts
@@ -1,21 +1,22 @@
-import { z } from "zod"
+import { success, z, ZodError } from "zod";
 import { title } from "node:process";
-import { createTaskSchema } from "../schemas/task.schemas";
+import { taskSchema } from "../schemas/tasks.schemas";
 import { NextFunction, Request, Response } from "express";
 
 export const validate = (schema: z.ZodObject<any>) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const bodyRequest = req.body
-            await schema.parseAsync(bodyRequest);
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const bodyRequest = req.body;
+      await schema.parseAsync(bodyRequest);
 
-            next()
-        } catch (error) {
-            if(error instanceof z.ZodError) {
-                return res
-                  .status(400)
-                  .json({ code: 400, sucess: false, data: error.issues });
-            }
-        }
+      next();
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res
+          .status(400)
+          .json({ code: 400, sucess: false, data: error.issues });
+      }
     }
-}
+  };
+};
+
