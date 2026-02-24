ebd18eae069e40c56551cb574f91fffa7748fa00
Keyth Alves <keythalvesferreia@gmail.com>
2026-02-24 05:18:04 -0300

    feat: user data update created

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../configs/db.config";
 import { Users } from "../models/users.model";
-import bcrypt from "bcrypt";
+import { Not } from "typeorm";
+import bcrypt, { compareSync } from "bcrypt";
+import { email, success } from "zod";
 export class UsersControllers {
   async getUsers(req: Request, res: Response) {
     try {
@@ -16,8 +18,8 @@ export class UsersControllers {
   async createUsers(req: Request, res: Response) {
     try {
       const userEmailExist = await AppDataSource.manager.findOneBy(Users, {
-        email: req.body.email
-      })
+        email: req.body.email,
+      });
 
       if (userEmailExist) {
         return res.status(409).json({
@@ -26,15 +28,15 @@ export class UsersControllers {
           message: "Usuário já cadastrado",
         });
       }
-      
+
       const passwordHash = await bcrypt.hash(req.body.password, 10);
 
       const newUser = AppDataSource.manager.create(Users, {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
-        password: passwordHash
-      })
+        password: passwordHash,
+      });
       await AppDataSource.manager.save(newUser);
 
       return res.status(201).json({
@@ -50,8 +52,62 @@ export class UsersControllers {
       });
     }
   }
-//update - validar se usuario existe
-//update - se quiser mudar a senha, verificar senha antiga e se estiver ok, pode alterar pela senha nova
-//update - caso alterar email, verificar se email já existe no banco
-//sem validação para nome e sobrenome
+
+  async updateUsers(req: Request, res: Response) {
+    const userID = await AppDataSource.manager.findOneBy(Users, {
+      id: Number(req.params.id),
+    });
+
+    if (!userID) {
+      return res
+        .status(404)
+        .json({ code: 404, success: false, message: "user not found" });
+    }
+
+    // if (req.body.password) {
+    //   const passwordResp = req.body.password;
+
+    //   const passwordHash = userID.password;
+
+    //   const newPassword = compareSync(passwordResp, passwordHash);
+
+    //   if (newPassword) {
+    //     const userNewPassword = await AppDataSource.manager.findOneBy(Users, {
+    //       password: req.body.password,
+    //     });
+    //   } else {
+    //     return res.status(409).json({
+    //       code: 409,
+    //       sucess: false,
+    //       message: "Não autorizado",
+    //     });
+    //   }
+    // }
+
+    if (req.body.email) {
+      const userChangeEmail = await AppDataSource.manager.findOneBy(Users, {
+        email: req.body.email,
+      });
+
+      if (userChangeEmail) {
+        return res.status(409).json({
+          code: 409,
+          sucess: false,
+          message: "Email já cadastrado",
+        });
+      }
+    }
+
+    const modificationUser = AppDataSource.manager.merge(
+      Users,
+      userID,
+      req.body,
+    );
+    await AppDataSource.manager.save(modificationUser);
+
+    return res.status(200).json({
+      code: 200,
+      message: "Dados alterados com sucesso",
+    });
+  }
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { UsersControllers } from "../controllers/users.controllers";
 import { validate } from "../middleware/validate-schema.middleware";
-import { usersSchema } from "../schemas/users.schemas";
+import { usersSchema, usersSchemaOptional } from "../schemas/users.schemas";
 
 const usersRouter = Router();
 const usersControllers = new UsersControllers();
 
 usersRouter.get("/getUsers", usersControllers.getUsers);
-usersRouter.post("/createUser", validate(usersSchema), usersControllers.createUsers)
+usersRouter.post("/createUser", validate(usersSchema), usersControllers.createUsers);
+usersRouter.patch("/updateUser/:id",validate(usersSchemaOptional), usersControllers.updateUsers)
 
 export { usersRouter };
